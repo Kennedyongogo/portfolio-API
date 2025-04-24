83b49319293176a222c598cfb49bc0823bb1c3db
mutetid <mutetidanz@gmail.com>
2025-04-24 16:09:25 +0300

    Refactor Project model to use named import for sequelize instance

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,5 +1,5 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+const { DataTypes, Sequelize } = require("sequelize");
+const { sequelize } = require("../config/database");
 
 const Project = sequelize.define(
   "project",
